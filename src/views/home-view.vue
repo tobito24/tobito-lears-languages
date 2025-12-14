e418e764a1c63c7198059f5a59a930484cacbc54
Tobias Berth <tobias.berth@smail.th-koeln.de>
2025-12-14 05:45:25 +0100

    feat: add vocabulary display with translations and enhance layout in home view

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Divider from 'primevue/divider';
 import { useVocab } from '@/composables/useVocab'
 import { useI18n } from 'vue-i18n'
 
@@ -17,15 +18,18 @@ const { vocab, isLoading, error } = useVocab()
             'text-2xl',
             'font-bold',
             'text-surface-950',
+            'text-center'
         ]">
             {{ t('home.welcomeMessage') }}
         </p>
         <p :class="[
             'text-lg',
-            'text-surface-950'
+            'text-surface-950',
+            'text-center'
         ]">
             {{ t('home.getStartedMessage') }}
         </p>
+        <Divider />
         <p v-if="!isLoading" :class="[
             'text-md',
             'text-surface-950'
@@ -43,6 +47,43 @@ const { vocab, isLoading, error } = useVocab()
             'text-surface-950'
         ]">
             {{ t('home.errorLoadingVocab', { msg: error }) }}
+        </div>
+        <div v-if="!isLoading">
+            <div :class="[
+                'grid',
+                'grid-cols-3',
+                'gap-2',
+            ]">
+                <div v-for="item in vocab" :key="item.id" :class="[
+                    'border',
+                    'border-surface-300',
+                    'rounded-xl',
+                    'p-2',
+                    'bg-surface-100',
+                    'hover:bg-surface-200',
+                    'hover:shadow-md',
+                    'hover:border-primary-400',
+                ]">
+                    <div :class="[
+                        'font-semibold',
+                        'text-surface-900',
+                    ]">
+                        {{ item.translations['en'].text }}
+                    </div>
+                    <div :class="[
+                        'text-surface-700',
+                    ]">
+                        {{ item.translations['de'].text }}
+                    </div>
+                    <div :class="[
+                        'text-surface-700',
+                        'italic',
+                    ]">
+                        {{ item.translations['es'].text }}
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
